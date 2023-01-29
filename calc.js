f28e4558002861e018e3b94d44fe78d9f2353c60
user1 <user1.gmail.com>
2023-01-29 18:26:52 +0530

    js done

--- calc.js
+++ calc.js
@@ -3,7 +3,29 @@ const buttons=document.querySelectorAll(".btn");
 const inp=document.querySelector("#txt");
 for(let button of buttons){
     button.addEventListener("click",function(){
-        str+=(this.textContent);
-        txt.value=str;
+        let btnContent=(this.textContent);
+        if(btnContent=='X'){
+            btnContent='*';
+            str+=btnContent;
+            txt.value=str;
+        }
+        else if (btnContent=='Back'){
+            str=str.slice(0,str.length-1);
+            txt.value=str;
+
+        }
+        else if(btnContent=='Clr'){
+            str='';
+            txt.value=str;
+        }
+        else if(btnContent=='='){
+            str=eval(str);
+            txt.value=str;
+        }
+        else{
+            str+=btnContent;
+            txt.value=str;
+        }
+        
     })
 }
